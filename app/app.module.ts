f624b854f2cc3b7e97213a1441c79aaff6c09d25
AAKASH HANDA <AAKASH HANDA>
2017-12-21 20:12:11 +0530

    A

---
+++ app/app.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {CarComponent} from './car.component';
+import { ProductComponent } from "./products/product-list.component";
+
+@NgModule({
+  imports:[
+    BrowserModule
+  ],
+  declarations:[
+    AppComponent,
+    CarComponent,
+    ProductComponent
+  ],
+  bootstrap:[AppComponent]
+})
+
+
+export class AppModule{}
---
+++ app/car.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'car-app',
+    template:`<div>
+        <h2>This is car</h2>
+    </div>`
+})
+
+export class CarComponent{
+
+}
